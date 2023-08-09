b1e57f9571a7f0d26c37df75c5eb0137bf4e0619
yhs01459 <50163224+yhs01459@users.noreply.github.com>
2023-08-09 18:44:38 +0900

    링크, 네비게이션 기능 추가

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation></MainNavigation>
       <Routes>
          <Route path='/' element={<AllMeetupsPage />}></Route>
          <Route path='/favorites' element={<FavoritesPage />}></Route>
