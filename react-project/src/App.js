f67c5bd1ee9abdffc75df6e3ad21ca67bbba164d
yhs01459 <50163224+yhs01459@users.noreply.github.com>
2023-08-10 20:06:40 +0900

    Layout 컴포넌트 추가
     app 컴포넌트를 커스텀 레이아웃으로 래핑

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -3,17 +3,18 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
 import MainNavigation from "./components/MainNavigation";
+import Layout from "./components/ui/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation></MainNavigation>
+    <Layout>
+     
       <Routes>
          <Route path='/' element={<AllMeetupsPage />}></Route>
          <Route path='/favorites' element={<FavoritesPage />}></Route>
         <Route path='/new-meetup' element={<NewMeetupPage />}></Route>
       </Routes>
-    </div>
+      </Layout>
   );
 }
 
---
+++ react-project/src/components/ui/Layout.js
@@ -0,0 +1,14 @@
+import classes from './Layout.module.css';
+import MainNavigation from '../MainNavigation';
+const Layout = (props) => {
+
+    return (
+        <div>
+             <MainNavigation></MainNavigation>
+            <main className={classes.main}>
+                {props.children}
+            </main>
+        </div>
+    )
+}
+export default Layout;
