d4348097ab98b769d5a6cd3feb71009d953a802d
yhs01459 <50163224+yhs01459@users.noreply.github.com>
2023-08-11 17:00:24 +0900

    즐겨찾기 페이지 구현
     - context를 이용해 즐겨찾기 페이지 구현

--- react-project/src/components/MainNavigation.js
+++ react-project/src/components/MainNavigation.js
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import classes from './MainNavigation.module.css';
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+
 
 function MainNavigation(){
+
+    const favoriesCtx = useContext(FavoritesContext);
     return(
         <header className={classes.header}>
             <div className={classes.logo}>My Meetup</div>
@@ -11,7 +16,9 @@ function MainNavigation(){
                         <Link to='/'>All Meetups</Link>
                     </li>
                     <li>
-                        <Link to='/favorites'>Favorites</Link>
+                        <Link to='/favorites'>Favorites
+                        <span className={classes.badge}>{favoriesCtx.totalFavorites}</span>
+                        </Link>
                     </li>
                     <li>
                         <Link to='/new-meetup'>New Meetup</Link>
--- react-project/src/pages/Favorites.js
+++ react-project/src/pages/Favorites.js
@@ -1,4 +1,20 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/MeetupList";
+
 function FavoritesPage(){
-    return <div>Favorites page</div>
+    const favoriesCtx = useContext(FavoritesContext);
+    let content;
+    if(favoriesCtx.totalFavorites ===0){
+        content = <p>즐겨찾기 항목이 비어있습니다.</p>
+
+    } else {
+        content = <MeetupList meetups ={favoriesCtx.favorites}></MeetupList>
+    }
+
+    return (<section>
+        <h1>My Favorites</h1>
+        {content}
+        </section>);
 }
 export default FavoritesPage;
