283f95c8609cf9dfd8b82886d10c472e32210c33
yhs01459 <50163224+yhs01459@users.noreply.github.com>
2023-08-14 17:14:47 +0900

    커스텀 404 페이지 추가

---
+++ next-project/app/not-found.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+const NotFoundPage = () => {
+    return <div>
+        <h1> not found page </h1>
+        <Link href ="/">return to homepage</Link>
+    </div>
+}
+export default NotFoundPage;
