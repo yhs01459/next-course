63b54f9050b89e870c4f0723f955f68557e22b31
yhs01459 <50163224+yhs01459@users.noreply.github.com>
2023-08-16 00:12:11 +0900

    필터 컴포넌트 css 적용
     - EventFilter.module.css

--- next-project/components/events/EventFilter.js
+++ next-project/components/events/EventFilter.js
@@ -1,6 +1,7 @@
 "use client";
 import Button from "../ui/Button";
 import { useRef } from "react";
+import classes from './EventFilter.module.css';
 
 const EventFilter = (props) => {
   const getYear = useRef();
@@ -16,22 +17,23 @@ const EventFilter = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div>
-        <div>
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.controls}>
+        <div className={classes.control}>
           <label htmlFor="year">year</label>
           <select id="year" ref={getYear}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
-      </div>
-      <div>
+
+      <div className={classes.control}>
         <label htmlFor="month">month</label>
         <select id="month" ref={getMonth}>
           <option value="04">4월</option>
           <option value="05">5월</option>
         </select>
+      </div>
       </div>
       <Button>Filter Events</Button>
     </form>
