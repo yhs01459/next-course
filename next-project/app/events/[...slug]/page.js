5b02d69772554351fa168eae2e99e1e486bac94e
yhs01459 <50163224+yhs01459@users.noreply.github.com>
2023-08-16 00:42:38 +0900

    필터 페이지 구현
     - [sulg]/page.js

--- next-project/app/events/[...slug]/page.js
+++ next-project/app/events/[...slug]/page.js
@@ -1,9 +1,22 @@
-
+'use client';
+import EventList from "@/components/events/EventList";
+import { usePathname } from "next/navigation";
+import { getFilteredEvents } from "@/dummy-data";
 const EventFilterPage = () => {
+   const path = usePathname();
    
+   const getYearBySegment = path.slice(8,12);
+   const getMonthBySegment = path.slice(14);
+   const numYear = +getYearBySegment;
+   const numMonth = +getMonthBySegment;
+   const fiteredEvents = getFilteredEvents({
+    year:numYear,
+    month:numMonth
+   });
+
     return(
       <div>
-        <h1>WELCOME EVENT FILTER PAGE</h1>
+        <EventList item={fiteredEvents}></EventList>
       </div>
     )
   }
